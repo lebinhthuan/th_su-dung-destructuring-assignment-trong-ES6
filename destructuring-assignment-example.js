6537a283e68ba2620d697a6b30c486a78d47302f
lebinhthuan <104605059+lebinhthuan@users.noreply.github.com>
2022-06-01 22:46:14 +0700

    update  - Buoc 7

--- destructuring-assignment-example.js
+++ destructuring-assignment-example.js
@@ -1,8 +1,8 @@
 
-let marks = {x: 21, y: -34, z: 47 };
 
-const { x, y, z } = marks; // x = 21, y = -34, z = 47
-
-console.log(x);
-console.log(y);
-console.log(z);
+const marks = {
+    section1: { alpha: 15, beta: 16},
+    section2: { alpha: -31, beta: 19 }
+};
+const { section1 : { alpha: alpha1, beta: beta1 }} = marks;
+console.log(alpha1, beta1); // 15, 16
